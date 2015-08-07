bf15921ffbfa24a600293daa0403b929fc818650
Qicheng Lao <Qicheng.Lao@gmail.com>
2015-08-07 18:15:47 -0400

    new start

--- application/models/country_model.php
+++ application/models/country_model.php
@@ -5,7 +5,6 @@
  * Date: 2015-08-05
  * Time: 10:07 PM
  */
-
 class Country_model extends CI_Model
 {
     /**
@@ -17,7 +16,6 @@ class Country_model extends CI_Model
      */
     public function get($country_name = null)
     {
-
         if ($country_name === null) {
             $query = $this->db->get('country');
         } else if (is_array($country_name)) {
@@ -25,22 +23,16 @@ class Country_model extends CI_Model
         } else {
             $query = $this->db->get_where('country', ['name' => $country_name]);
         }
-
         return $query->result_array();
     }
-
     public function insert($data)
     {
         $this->db->insert('country', $data);
         return $this->db->insert_id();
     }
-
     public function delete($country_name)
     {
         $this->db->delete('country', ['name' => $country_name]);
         return $this->db->affected_rows();
     }
-
-
-
 }
